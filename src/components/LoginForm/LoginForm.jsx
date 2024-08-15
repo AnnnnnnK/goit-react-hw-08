51ded4e2c86dcdd863730cb1c05989d5b2a35957
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2024-08-15 17:08:53 +0200

    add form styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,33 +1,98 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, useFormik } from "formik";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/auth/operations";
+import { Button, Card, CardContent, Container, TextField } from "@mui/material";
+import css from "./LoginForm.module.css";
+import { NavLink } from "react-router-dom";
 // import { Form } from "react-router-dom";
 
 const LoginForm = () => {
-  const initialValues = {
-    email: "",
-    password: "",
-  };
-
   const dispatch = useDispatch();
-  const handleSubmit = (values, options) => {
-    console.log(values);
-    dispatch(login(values));
-    options.resetForm();
-  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    //  validationSchema: validationSchema,
+    onSubmit: (values, options) => {
+      console.log(values);
+      dispatch(login(values));
+      options.resetForm();
+    },
+  });
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form>
-        <Field name="email" type="text" placeholder="Enter your email" />
-        <Field
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-        />
-        <button type="submit">Login</button>
-      </Form>
-    </Formik>
+    <Container maxWidth="sm">
+      <Card variant="outlined">
+        <CardContent>
+          <form onSubmit={formik.handleSubmit} className={css.formContainer}>
+            <h2>Log in to your account</h2>
+            <div className={css.inputsContainer}>
+              <TextField
+                fullWidth
+                size="small"
+                name="email"
+                label="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                // id="password"
+                size="small"
+                name="password"
+                label="Password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                sx={{ mb: 2 }}
+              />
+            </div>
+            <Button
+              sx={{ ml: 56 }}
+              color="secondary"
+              variant="contained"
+              size="small"
+              type="submit"
+            >
+              Log In
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </Container>
+
+    // </div>
   );
 };
+
+// const LoginForm = () => {
+//   const initialValues = {
+//     email: "",
+//     password: "",
+//   };
+
+//   const dispatch = useDispatch();
+//   const handleSubmit = (values, options) => {
+//     console.log(values);
+//     dispatch(login(values));
+//     options.resetForm();
+//   };
+//   return (
+//     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+//       <Form>
+//         <Field name="email" type="text" placeholder="Enter your email" />
+//         <Field
+//           name="password"
+//           type="password"
+//           placeholder="Enter your password"
+//         />
+//         <button type="submit">Login</button>
+//       </Form>
+//     </Formik>
+//   );
+// };
 
 export default LoginForm;
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,33 +1,99 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, useFormik } from "formik";
 import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
+import { Button, Card, CardContent, Container, TextField } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 const RegistrationForm = () => {
-  const initialValues = {
-    name: "",
-    email: "",
-    password: "",
-  };
   const dispatch = useDispatch();
 
-  const handleSubmit = (values, options) => {
-    console.log(values);
-    dispatch(register(values));
-    options.resetForm();
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    //  validationSchema: validationSchema,
+    onSubmit: (values, options) => {
+      console.log(values);
+      dispatch(register(values));
+      options.resetForm();
+    },
+  });
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form>
-        <Field name="name" placeholder="Enter your name" />
-        <Field name="email" type="text" placeholder="Enter your email" />
-        <Field
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-        />
-        <button type="submit">Login</button>
-      </Form>
-    </Formik>
+    <Container maxWidth="sm">
+      <Card variant="outlined">
+        <CardContent>
+          <form onSubmit={formik.handleSubmit}>
+            <h2>Create an account</h2>
+            <TextField
+              fullWidth
+              name="name"
+              label="Name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              size="small"
+              margin="normal"
+            />
+            <TextField
+              fullWidth
+              // id="email"
+              name="email"
+              label="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              size="small"
+              margin="normal"
+              // onBlur={formik.handleBlur}
+              // error={formik.touched.email && Boolean(formik.errors.email)}
+              // helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              fullWidth
+              // id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              size="small"
+              margin="normal"
+            />
+            <Button color="secondary" variant="contained" type="submit">
+              Register
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 export default RegistrationForm;
+
+// const initialValues = {
+//   name: "",
+//   email: "",
+//   password: "",
+// };
+// const dispatch = useDispatch();
+// const handleSubmit = (values, options) => {
+//   console.log(values);
+//   dispatch(register(values));
+//   options.resetForm();
+// };
+// return (
+//   <div>
+//     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+//       <Form>
+//         <Field name="name" placeholder="Enter your name" />
+//         <Field name="email" type="text" placeholder="Enter your email" />
+//         <Field
+//           name="password"
+//           type="password"
+//           placeholder="Enter your password"
+//         />
+//         <button type="submit">Login</button>
+//       </Form>
+//     </Formik>
+//   </div>
+// );
